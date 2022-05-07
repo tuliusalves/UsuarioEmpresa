d7978cb65544538b0a4eb46f6564d409db9317fc
Túlius <tulius_sr@outlook.com>
2022-05-07 16:20:00 -0300

    Adicionando as entidades 'emailIgnorado', 'Empresa' e 'Usuario'

--- UsuarioEmpresa/src/br/com/usuarioempresa/model/EmailsIgnorados.java
+++ UsuarioEmpresa/src/br/com/usuarioempresa/model/EmailsIgnorados.java
@@ -1,5 +1,14 @@
 package br.com.usuarioempresa.model;
 
 public class EmailsIgnorados {
+	private String emailIgnorado;
 
+	public String getEmailIgnorado() {
+		return emailIgnorado;
+	}
+
+	public void setEmailIgnorado(String emailIgnorado) {
+		this.emailIgnorado = emailIgnorado;
+	}
+	
 }
--- UsuarioEmpresa/src/br/com/usuarioempresa/model/Empresa.java
+++ UsuarioEmpresa/src/br/com/usuarioempresa/model/Empresa.java
@@ -1,5 +1,59 @@
 package br.com.usuarioempresa.model;
 
 public class Empresa {
+	private String nomeFantasia;
+	private int cnpj;
+	private String dataFundacao;
+	private String email;
+	private int telefone;
+	private int cep;
+	private String endereco;
+	
+	
+	
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public int getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(int cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getDataFundacao() {
+		return dataFundacao;
+	}
+	public void setDataFundacao(String dataFundacao) {
+		this.dataFundacao = dataFundacao;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public int getCep() {
+		return cep;
+	}
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
 
 }
